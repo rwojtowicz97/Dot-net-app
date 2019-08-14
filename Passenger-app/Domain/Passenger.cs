2213a488a1df3cb7aba7a542ef33a6f0283f205c
Robert <43409796+rwojtowicz97@users.noreply.github.com>
2019-08-14 10:52:56 +0200

    Create Passenger.cs

---
+++ Passenger-app/Domain/Passenger.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Passenger.Domain
+{
+    public class Passenger
+    {
+        public Guid Id { get; protected set; }
+        public Guid UserId { get; protected set; }
+        public Node Address { get; protected set; }
+    }
+}
