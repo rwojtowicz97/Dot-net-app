58352499acf713a02fe389f1b5390297e37529a3
rwojtowicz97 <robivw508@gmail.com>
2019-08-22 08:45:02 +0200

    Made Driver async

--- Passenger-app/Passenger.Api/Controllers/UsersController.cs
+++ Passenger-app/Passenger.Api/Controllers/UsersController.cs
@@ -19,7 +19,7 @@ namespace Passenger.Api.Controllers {
         public async Task<UserDto> Get(string email) 
             => await _userService.GetAsync(email);
 
-        [HttpGet ("")]
+        [HttpPost]
         public async Task Post([FromBody]CreateUser request)
           => await _userService.RegisterAsync(request.Email, request.Username, request.Password);
         
