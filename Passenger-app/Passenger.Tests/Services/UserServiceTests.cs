0bc5d208c4cd6978e72d1bdd5a51c6b3b7e495b5
rwojtowicz97 <robivw508@gmail.com>
2019-08-26 12:09:12 +0200

    Fixed problem with usings (moq, xunit, fluentassertions)

--- Passenger-app/Passenger.Tests/Services/UserServiceTests.cs
+++ Passenger-app/Passenger.Tests/Services/UserServiceTests.cs
@@ -1,16 +1,15 @@
 using System.Threading.Tasks;
+using Passenger.Core.Repositories;
+using AutoMapper;
+using Passenger.Infrastructure.Services;
+using Passenger.Core.Domain;
 using FluentAssertions;
 using Xunit;
 using Moq;
-using Passenger.Core.Repositories;
-using AutoMapper;
-using Passenger.Infrastructure.Repositories;
-using Passenger.Infrastructure.Services;
-using Passenger.Core.Domain;
 
 namespace Passenger.Tests.Services
 {
-    
+
     public class UserServiceTests
     {
         [Fact]
