650bd9653bc9556da5e5a509a778d5692341152d
Martyna Niespodziany <tynka@MacBook-Air-Martyna.local>
2019-08-27 20:59:41 +0200

    started writing firts test

--- Passenger-app/Passenger.Tests.EndToEnd/Controllers/UsersControllerTests.cs
+++ Passenger-app/Passenger.Tests.EndToEnd/Controllers/UsersControllerTests.cs
@@ -1,6 +1,7 @@
 using System.Net.Http;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.AspNetCore.Hosting;
+using Passenger.Api;
 
 namespace Passenger.Tests.EndToEnd.Controllers
 {
@@ -10,7 +11,14 @@ namespace Passenger.Tests.EndToEnd.Controllers
         private readonly HttpClient _client;
         public UsersControllerTests()
         {
-            _server = new TestServer(new WebHostBuilder().)
+            _server = new TestServer(new WebHostBuilder().
+                    UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        public async Task given_valid_emaill_user_should_exists()
+        {
+            
         }
     }
 }
