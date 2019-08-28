3f1f9a7b3ef81b2076cb5c0418acf3a6a0056565
rwojtowicz97 <robivw508@gmail.com>
2019-08-28 11:58:56 +0200

    Made first test EndToEnd, added package reference Microsoft.AspNetCore.App to Tests.EndToEnd

--- Passenger-app/Passenger.Tests.EndToEnd/Controllers/UsersControllerTests.cs
+++ Passenger-app/Passenger.Tests.EndToEnd/Controllers/UsersControllerTests.cs
@@ -1,7 +1,12 @@
 using System.Net.Http;
-using Microsoft.AspNetCore.TestHost;
 using Microsoft.AspNetCore.Hosting;
 using Passenger.Api;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+using Passenger.Infrastructure.DTO;
+using FluentAssertions;
+using Xunit;
 
 namespace Passenger.Tests.EndToEnd.Controllers
 {
@@ -15,10 +20,17 @@ namespace Passenger.Tests.EndToEnd.Controllers
                     UseStartup<Startup>());
             _client = _server.CreateClient();
         }
-
+        [Fact]
         public async Task given_valid_emaill_user_should_exists()
         {
-            
+            var email = "user1@mail.com";
+            var response = await _client.GetAsync($"users/{email}");
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var user = JsonConvert.DeserializeObject<UserDto>(responseString);
+
+            user.Email.Should().BeEquivalentTo(email);
         }
     }
 }
