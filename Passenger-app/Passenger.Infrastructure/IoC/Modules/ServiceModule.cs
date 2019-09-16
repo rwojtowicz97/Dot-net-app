a14d4bd8c4056922853e4e69115ae570fe7c5477
rwojtowicz97 <robivw508@gmail.com>
2019-09-16 17:51:24 +0200

    trying to implemeny jwt

--- Passenger-app/Passenger.Infrastructure/IoC/Modules/ServiceModule.cs
+++ Passenger-app/Passenger.Infrastructure/IoC/Modules/ServiceModule.cs
@@ -20,6 +20,10 @@ namespace Passenger.Infrastructure.IoC.Modules
             builder.RegisterType<Encrypter>()
                     .As<IEncrypter>()
                     .SingleInstance();
+            
+            builder.RegisterType<JwtHandler>()
+                    .As<JwtHandler>()
+                    .SingleInstance();
         }
     }
 }
