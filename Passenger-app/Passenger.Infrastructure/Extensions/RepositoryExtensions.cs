46cb7f62de97e4c21b7475845df740e16990c529
rwojtowicz97 <robivw508@gmail.com>
2019-11-25 17:30:26 +0100

    One more save

--- Passenger-app/Passenger.Infrastructure/Extensions/RepositoryExtensions.cs
+++ Passenger-app/Passenger.Infrastructure/Extensions/RepositoryExtensions.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Threading.Tasks;
+using Passenger.Core.Repositories;
+
 namespace Passenger.Infrastructure.Extensions
 {
     public static class RepositoryExtensions
     {
-        
+        public static async Task<Driver> GetOrFailAsync(this IDriverRepository repository, Guid userId)
     }
 }
